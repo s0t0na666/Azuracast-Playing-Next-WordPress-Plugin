44ecb9d53528c8e63193482335d29be336f3052d
s0t0na666 <40716724+s0t0na666@users.noreply.github.com>
2025-02-17 02:05:37 +0100

    Update Azuracast-playing-next-websocket.php

--- Azuracast-playing-next-websocket.php
+++ Azuracast-playing-next-websocket.php
@@ -15,14 +15,11 @@ class AzuraCast_WebSocket {
     private $azuracast_url;
     private $station_name;
     private $show_album_art;
-    private $refresh_interval;
 
     public function __construct() {
         $this->azuracast_url = get_option('azuracast_url', '');
         $this->station_name = get_option('azuracast_station_name', '');
         $this->show_album_art = get_option('azuracast_show_album_art', 'yes');
-        $this->refresh_interval = get_option('azuracast_refresh_interval', '5000');
-
         add_shortcode('azura_playing_next', [$this, 'display_playing_next']);
         add_action('admin_menu', [$this, 'add_admin_menu']);
         add_action('admin_init', [$this, 'register_settings']);
@@ -43,7 +40,6 @@ class AzuraCast_WebSocket {
         register_setting('azuracast-websocket-settings', 'azuracast_url');
         register_setting('azuracast-websocket-settings', 'azuracast_station_name');
         register_setting('azuracast-websocket-settings', 'azuracast_show_album_art');
-        register_setting('azuracast-websocket-settings', 'azuracast_refresh_interval');
     }
 
     public function settings_page() {
@@ -71,10 +67,6 @@ class AzuraCast_WebSocket {
                             </select>
                         </td>
                     </tr>
-                    <tr valign="top">
-                        <th scope="row">Refresh Interval (ms)</th>
-                        <td><input type="number" name="azuracast_refresh_interval" value="<?php echo esc_attr(get_option('azuracast_refresh_interval')); ?>" min="1000" step="1000" /></td>
-                    </tr>
                 </table>
                 <?php submit_button(); ?>
             </form>
@@ -83,37 +75,40 @@ class AzuraCast_WebSocket {
     }
 
     public function fetch_playing_next() {
-        $ws_url = esc_js($this->azuracast_url);
-        $station = esc_js($this->station_name);
-        $refresh_interval = intval($this->refresh_interval);
-        $show_album_art = esc_js($this->show_album_art) === 'yes' ? 'true' : 'false';
-
-        $sub_message = json_encode(["subs" => ["station:$station" => new stdClass()]]);
+        $ws_url = "wss://" . $this->azuracast_url . "/api/live/nowplaying/websocket";
+        $station = "station:" . $this->station_name;
+        $sub_message = json_encode(["subs" => [$station => new stdClass()]]);
 
         $script = "
+            <style>
+                #azura-playing-next {
+                    display: flex;
+                    flex-wrap: nowrap;
+                    align-items: center;
+                }
+            </style>
             <script>
                 let ws;
                 function connectWebSocket() {
                     if (ws) ws.close();
-                    ws = new WebSocket('wss://$ws_url/api/live/nowplaying/websocket');
-                    ws.onopen = function() { ws.send($sub_message); };
+                    ws = new WebSocket('$ws_url');
+                    ws.onopen = function() { ws.send('$sub_message'); };
                     ws.onmessage = function(event) {
                         let data = JSON.parse(event.data);
                         if (data.pub && data.pub.data && data.pub.data.np) {
                             let playingNext = data.pub.data.np.playing_next;
                             if (playingNext && playingNext.song) {
                                 let nextSongHTML = '';
-                                if ($show_album_art && playingNext.song.art) {
-                                    nextSongHTML += `<img src='${playingNext.song.art}' onerror='this.style.display=\"none\"' style='width:50px; height:50px; margin-right:10px;'>`;
+                                if ('" . $this->show_album_art . "' === 'yes' && playingNext.song.art) {
+                                    nextSongHTML += `<img src='` + playingNext.song.art + `' onerror=\"this.style.display='none'\" style='width:50px; height:50px; margin-right:10px;'>`;
                                 }
-                                nextSongHTML += playingNext.song.artist + ' - ' + playingNext.song.title;
+                                nextSongHTML += playingNext.song.artist.replace(/\b\w/g, char => char.toUpperCase()) + ' - ' + playingNext.song.title.replace(/\b\w/g, char => char.toUpperCase());
                                 requestAnimationFrame(() => {
                                     document.getElementById('azura-playing-next').innerHTML = nextSongHTML;
                                 });
                             }
                         }
                     };
-                    ws.onerror = function() { setTimeout(connectWebSocket, $refresh_interval); };
                 }
                 connectWebSocket();
             </script>
